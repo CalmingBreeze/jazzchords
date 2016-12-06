8aba5cdceb39b6908db56ff9ab82f4b598daf552
morieff <morieff@yahoo.fr>
2016-12-06 17:08:08 +0100

    Finalizing GUI for visualizer : + Connect button and events to restore older design visualizer functionalities
    TODO : Handle validation and :active tags switching

--- javascript/onDomReady.js
+++ javascript/onDomReady.js
@@ -4,23 +4,23 @@ $(document).ready(function() {
 	//find where to add the keyboard
 	//var hardCodedSvgKeyboard = document.querySelector('#keyboard.panel');
 	test829 = new SVGKeyboard('test829','#visualKeyboard');
-	
+
 	//Create and insert it
 	//var n = document.importNode(test829.init(3),true);
     //document.body.insertBefore(n, hardCodedSvgKeyboard);
 	//document.body.appendChild(n);
 	//test829.bindSoundEvents();
-	
+
 	/*
 	var C4_note = new Note(48); //C4
 	var F4sharp_note = new Note(54); //F4#
 	var G5flat_note = new Note(66,-1); //G5b
-	
+
 	console.log(C4_note.noteName);
 	console.log(F4sharp_note.noteName);
 	console.log(G5flat_note.noteName);
 	*/
-	
+
 	/*
 	var test4 = new Chord('Cm7', 0);
 	console.log(test4);
@@ -31,12 +31,12 @@ $(document).ready(function() {
 	console.log(test4);
 	test4.getNotesByName();
 	*/
-	
+
 	//var test5 = new Chord('Chalfdim', 0, 3);
 	//test5.getNotesByChordName();
 	//console.log(test5);
 	//test829.playChord(test5);
-	
+
 	/*
 	//CMaj7
 	test829.keys[12].strike();
@@ -44,7 +44,7 @@ $(document).ready(function() {
 	test829.keys[19].strike();
 	test829.keys[23].strike();
 	*/
-	
+
 	/*
 	//play C7 in Oct4
 	test829.playNote(C4_note);
@@ -52,20 +52,10 @@ $(document).ready(function() {
 	test829.playNote(C4_note.fifth());
 	test829.playNote(C4_note.seventh());
 	*/
-	
+
 	//play C7 in Oct4
 	//test829.playChord("C7");
-	
-	//TODO remove DEBUG here
-	//attach button handler
-	function computeChord(){
-		var chordName = document.querySelector("input#debugChord");
-		computedChord = new Chord(chordName.value, 0, 3);
-		chordName.value = "";
-		test829.playChord(computedChord);
-		
-	}
-	
-	var el = document.querySelector("button#testChordButton"); 
-	el.addEventListener("click", computeChord, false);
+
+	//var el = document.querySelector("button#testChordButton");
+	//el.addEventListener("click", computeChord, false);
 });
